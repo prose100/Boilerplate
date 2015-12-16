eeddb3b40037088c4fd74155682453d73eac09e6
Paul Rose <prose100@hotmail.com>
2015-12-16 15:33:29 -0600

    Created src and dist folders for html, css, and js development.

---
+++ src/js/app.js
@@ -0,0 +1,27 @@
+/*!
+ * Start Bootstrap - Grayscale Bootstrap Theme (http://startbootstrap.com)
+ * Code licensed under the Apache License v2.0.
+ * For details, see http://www.apache.org/licenses/LICENSE-2.0.
+ */
+
+$(document).ready(function(){
+    // fade in and fade out
+    $(function () {
+        $(window).scroll(function () {
+            if ($(this).scrollTop() > 50) {
+                $('#uparrow').fadeIn();
+            } else {
+                $('#uparrow').fadeOut();
+            }
+        });
+ 
+        // scroll body to 0px on click
+        $('#uparrow').click(function () {
+            $('body,html').animate({
+                scrollTop: 0
+            }, 800);
+            return false;
+        });
+    });
+ 
+});
